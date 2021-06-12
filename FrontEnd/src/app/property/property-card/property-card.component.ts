5d5e07371a8d6fc04484e90c8c9bbc1e1f395201
deepak <vndeepak1994@gmail.com>
2021-06-12 19:53:06 +0530

    PropertyBinding

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
+  @Input() property: any
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
 
-    
   }
 
 }
--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+         "Id":1,
+         "Name":"Birla House",
+         "Type":"House",
+         "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Virat House",
+      "Type":"House",
+      "Price":22000
+    },
+    {
+  "Id":3,
+  "Name":"Yuvi House",
+  "Type":"House",
+  "Price":19000
+},
+{
+  "Id":4,
+  "Name":"Mahi House",
+  "Type":"House",
+  "Price":16000
+},
+{
+  "Id":5,
+  "Name":"Sehwag House",
+  "Type":"House",
+  "Price":14000
+},
+{
+  "Id":5,
+  "Name":"Sachin House",
+  "Type":"House",
+  "Price":11000
+}
+  ]
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
